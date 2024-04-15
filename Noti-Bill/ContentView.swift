26e249b3b33e4b48490d9a5ca470a8555588fd67
TaninDean <reiodean.dean@gmail.com>
2024-04-15 13:49:15 +0700

    Initial Commit

---
+++ Noti-Bill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Noti-Bill
+//
+//  Created by ธนิน ผิวเหลืองสวัสดิ์ on 15/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Noti-Bill/Noti_BillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Noti_BillApp.swift
+//  Noti-Bill
+//
+//  Created by ธนิน ผิวเหลืองสวัสดิ์ on 15/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Noti_BillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
